7dfbdb0b1d0b14a03526f1cdf24bfd86b953aedb
Asplap3277 <mmohammedhamza333@gmail.com>
2022-12-01 12:09:48 +0530

    Implementation on various uses of  const keyword

---
+++ Constants.js
@@ -0,0 +1,37 @@
+ /*const name = { username : 'Muhammed' 
+              , age : '22'
+              , dob : '19/05/2000'    
+              }
+console.log(name)              
+name.age = '23' ;
+name.username = 'Hamza';
+name.dob = '11/03/1999';
+console.log(name)
+*/
+
+/*  Type error  example
+const age =  {age1:23 } 
+age = 22 
+console.log(age)
+age.age1 = 22
+console.log(age)
+*/
+
+/*    Arrray
+const mat = [22,23,24,25]
+mat.push(21)
+console.log(mat)
+mat.pop(23)
+console.log(mat)
+*/
+
+/*  For Of  Const and let
+const num = [10 , 20 ,30 ] 
+for(let n of num){
+      n = 22
+      num.n = 20 
+      console.log(n)
+}
+console.log(num)
+
+*/
