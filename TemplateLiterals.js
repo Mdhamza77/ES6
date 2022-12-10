c999c4b881272690f5cf11749bbaaa78e39dd37d
Asplap3277 <mmohammedhamza333@gmail.com>
2022-12-10 16:49:50 +0530

    Implementations on Template Literals and its uses

--- TemplateLiterals.js
+++ TemplateLiterals.js
@@ -6,7 +6,7 @@ let message = `Hello ${customer.name} want\'s to buy ${card.product} which cost
 price is ${card.amount} sum of total quantity price is ${card.amount * card.qtyprice}`
 console.log(message)
 
-
+// Practice Template
 const Strings = ["That","Its the","."]
 const person = "Mike Tyson"
 const age = 22
@@ -21,5 +21,19 @@ function my(Strings,personEx,ageEx){
      return `${st} ${personEx} ${str} ${ageEx}${strr}`
      
 }
+console.log(my(Strings,"Jamal",77))
 
-console.log(my(Strings,"Jamal",77));
+// Raw Method's 
+const str = String.raw`Hi\n${2 + 3}!`;
+Array.from(str).join(",");
+
+// Raw String Access  
+const filePath = String.raw`C:\Development\profil\about.html`;
+
+console.log(`The file was uploaded from: ${filePath}`);
+// Custom Interpolation  
+const bar = 55 
+const baz = 55 
+const quu = "This is to check Custom interpolation using String Literals" 
+console.log(`http://example.com/foo?bar=${bar + baz}&quux=${quu}`)
+
